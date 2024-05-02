b249bb89a1e46e3943c21fdf365c3931bbe1f27b
Russakura8 <vbifntht@gmail.com>
2024-05-02 16:38:33 +0300

    В общем и целом код больше не будет меняться

--- Clustering.py
+++ Clustering.py
@@ -1,9 +1,9 @@
 import numpy as np
-from scipy.cluster.hierarchy import linkage
-from scipy.spatial.distance import pdist
+from openpyxl import Workbook
 from sklearn import cluster, mixture
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+import openpyxl
 
 with open('normdata.txt', 'r') as f:
     _data = f.read()
@@ -18,32 +18,58 @@ students = eval(_data)
 
 
 data = np.array(data)
-
-
-
-clustering = cluster.DBSCAN(min_samples=3, eps=2.16).fit(data)
-#clustering = cluster.SpectralClustering().fit(data)
-#clustering = cluster.AgglomerativeClustering(n_clusters=6).fit(data)
-#clustering = cluster.KMeans(n_clusters=3).fit(data)
-#clustering = cluster.OPTICS(min_samples=3, eps=1.5).fit(data)
-#clustering = mixture.GaussianMixture(n_components = 3).fit(data)
-
-labels = clustering.labels_
-#labels = clustering.predict(data)
-
 np.set_printoptions(threshold=np.inf)
-#for i in range(len(labels)):
-#    if labels[i] == 1:
-#        print(students[i])
 
-print(labels)
-print(max(labels))
-print(np.count_nonzero(labels == -1))
+
+
+
+dbscan = cluster.DBSCAN(min_samples=3, eps=2.16).fit(data)
+spectral = cluster.SpectralClustering(n_clusters=6).fit(data)
+kmeans = cluster.KMeans(n_clusters=6).fit(data)
+gmm = mixture.GaussianMixture(n_components = 6).fit(data)
+birch = cluster.Birch().fit(data)
+
+
+methods = ["dbscan", "spectral", "kmeans", "gmm", "birch"]
 
 pca = PCA(n_components=2)
 pca.fit(data)
-data = pca.transform(data)
-#print(data)
+new_data = pca.transform(data)
 
-plt.scatter(data[:, 0], data[:, 1], c=labels, s=50, cmap='viridis')
-plt.show()
+for method in methods:
+    if method == "gmm":
+        labels = eval(f"{method}.predict(data)")
+    else:
+        labels = eval(f"{method}.labels_")
+
+
+    plt.scatter(new_data[:, 0], new_data[:, 1], c=labels, s=50, cmap='viridis')
+    plt.savefig(method + ".png")
+
+
+    wb = Workbook()
+
+
+    for i in range(max(labels) + 1):
+        ws = wb.create_sheet(str(i))
+        count = 1
+        for key in students[0]:
+            cell = ws.cell(row=1, column=count)
+            count += 1
+            cell.value = key
+        count = 2
+        for j in range(len(labels)):
+            if labels[j] == i:
+                count2 = 1
+                for key, value in students[j].items():
+                    cell =  ws.cell(row=count, column=count2)
+                    cell.value = value
+                    count2 += 1
+                count += 1
+
+    page  = wb['Sheet']
+    wb.remove(page)
+
+    wb.save(method + ".xlsx")
+    wb.close()
+
